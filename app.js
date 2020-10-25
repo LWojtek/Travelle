1efecf2c394e186efd26d2b8d2e20c04d2d29451
unknown <LWojtek>
2020-10-25 18:18:19 +0100

    sticky navbar

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const cloud1 = document.querySelector('.cloud1');
 const cloud2 = document.querySelector('.cloud2');
+const nabvar = document.querySelector('.main-head');
 
 window.addEventListener('scroll', () => {
-
+      console.log(scrollY);
       if (scrollY > 400) {
             cloud1.classList.add('moving-cloud-1')
             cloud2.classList.add('moving-cloud-2')
