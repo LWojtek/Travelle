7f29fb3a8ecdcf9af3fa1f03c4e2ba76b0621051
unknown <LWojtek>
2020-10-27 21:26:53 +0100

    added modal

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const cloud1 = document.querySelector('.cloud1');
 const cloud2 = document.querySelector('.cloud2');
 const nabvar = document.querySelector('.main-head');
+const btn = document.querySelector('.book-btn');
+
+
 
 window.addEventListener('scroll', () => {
       console.log(scrollY);
@@ -9,4 +12,17 @@ window.addEventListener('scroll', () => {
             cloud2.classList.add('moving-cloud-2')
       }
 
+})
+
+const closeBtn = document.querySelector('.modal-close');
+const modal = document.querySelector('.modal-bg');
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+      modal.classList.add('modal-active');
+})
+
+closeBtn.addEventListener('click', () => {
+      modal.classList.remove('modal-active');
 })
